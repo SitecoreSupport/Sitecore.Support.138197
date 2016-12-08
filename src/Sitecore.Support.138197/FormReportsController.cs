92d455d419e26da432548e0a745990800cab1bce
Ivan Sheyenko <ivs@sitecore.net>
2016-12-08 15:08:44 +0200

    patch created

---
+++ src/Sitecore.Support.138197/FormReportsController.cs
@@ -0,0 +1,36 @@
+﻿using Sitecore.WFFM.Analytics.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Web.Http.Filters;
+
+namespace Sitecore.Support.WFFM.Services.Requests.Controllers
+{
+    public class FormReportsExController : Sitecore.WFFM.Services.Requests.Controllers.FormReportsController
+    {
+        int maxJsonLength = 2097152;
+
+        public FormReportsExController() : base()
+        {
+
+        }
+
+        public FormReportsExController(IWfmDataProvider formsDataProvider, int MaxJsonLength) : base(formsDataProvider)
+        {
+            if (MaxJsonLength > 0)
+            {
+                this.maxJsonLength = MaxJsonLength;
+            }
+        }
+
+        [ValidateHttpAntiForgeryToken]
+        public new ActionResult GetFormFieldsStatistics(Guid id)
+        {
+            JsonResult result = base.GetFormFieldsStatistics(id) as JsonResult;
+            result.MaxJsonLength = this.maxJsonLength;
+            return result;
+        }
+    }
+}
---
+++ src/Sitecore.Support.138197/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.138197")]
+[assembly: AssemblyProduct("Sitecore.Support.138197")]
+[assembly: ComVisible(false)]
